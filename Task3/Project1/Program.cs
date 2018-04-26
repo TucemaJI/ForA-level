3a5f3b8025f4f52fe508a03a914fbf011a36d1fe
TucemaJI <bidi75518@gmail.com>
2018-04-26 14:07:03 +0300

    Task3

---
+++ Task3/Project1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double S;
+            //double s = 36.79;
+            //S = s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s + s;
+            //S += s / 1.2658226; //Косыль? Как по-другому?
+            int S=0,s=0;
+            Console.Write("Enter your number ");
+            Int32.TryParse(Console.ReadLine(), out s);
+            for (int i = 0;i < s;i++) { S += s; }
+            Console.WriteLine("Result "+S);
+            Console.ReadLine();
+            }
+    }
+}
