17d1f711615946f3fd7dc7b25778fcba94d4834e
TucemaJI <bidi75518@gmail.com>
2018-04-30 14:47:29 +0300

    Task4

---
+++ Task4/Project1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Rectangle
+    {
+        int h = 0;
+        int w = 0;
+        bool p = false;
+
+        public Rectangle(int h, int w)
+        {
+            this.h = h;
+            this.w = w;
+        }
+        public void S()
+        {
+            while (!p)
+            {
+                try
+                {
+                    int S = h * w;
+                    Console.WriteLine("Your h = " + h);
+                    Console.WriteLine("Your w = " + w);
+                    Console.WriteLine("Your S = " + S);
+                    break;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Enter a less number");
+                    p = true;
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool p = false;
+            while (!p)
+            {
+                try
+                {
+                    Console.Write("Write your h = ");
+                    int h = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Write your w = ");
+                    int w = Convert.ToInt32(Console.ReadLine());
+                    Rectangle R1 = new Rectangle(h, w);
+                    R1.S();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Enter a number next time");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Enter a less number next time");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task4/Project2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 0;
+            double n3;
+            bool s1 = false, s2 = false;
+            while (!s2)
+            {
+                while (!s1)
+                {
+                    try
+                    {
+                        Console.Write("Enter №1 = ");
+                        n1 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Enter a number");
+                        s1 = true;
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("Enter a less number");
+                        s1 = true;
+                    }
+                }
+                while (!s1)
+                {
+                    try
+                    {
+                        Console.Write("Enter №2 = ");
+                        n2 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Enter a number");
+                        s1 = true;
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("Enter a less number");
+                        s1 = true;
+                    }
+                }
+                if (s1 == false)
+                {
+                    n3 = (double)n1 / (double)n2;
+                    Console.WriteLine("Result = " + n3);
+                }
+                s1 = false;
+            }
+        }
+    }
+}
