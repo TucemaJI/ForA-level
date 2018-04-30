4bf68ebb190a7bbc71b7d5df853ecae8b5610cfe
TucemaJI <bidi75518@gmail.com>
2018-04-30 14:52:12 +0300

    Task6

---
+++ Task6/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Processor
+    {
+        public static void ProcessInput(int numint)
+        {
+            Console.WriteLine("Fizz");
+        }
+
+        public static void ProcessInput(double numdouble)
+        {
+            Console.WriteLine("Buzz");
+        }
+        public Processor(string enter)
+        {
+            int numint;
+            double numdouble;
+            if (int.TryParse(enter, out numint))
+            {
+                ProcessInput(numint);
+            }
+            else if (double.TryParse(enter, out numdouble))
+            {
+                ProcessInput(numdouble);
+            }
+            else { Console.WriteLine("Enter a number please"); }
+        }
+    }
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter a number:");
+                string enter = Console.ReadLine();
+                Processor first = new Processor(enter);
+            }
+        }
+    }
+}
---
+++ Task6/ConsoleApp2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter the Date");
+                string text = Console.ReadLine();
+                int delay = 0;
+                DateTime.TryParse(text, out DateTime date);
+
+                switch (date.DayOfWeek)
+                {
+                    case (DayOfWeek.Monday):
+                        {
+                            delay = 2; break;
+                        }
+                    case (DayOfWeek.Tuesday):
+                        {
+                            delay = 3; break;
+                        }
+                    case (DayOfWeek.Wednesday):
+                        {
+                            delay = 0; break;
+                        }
+                    case (DayOfWeek.Thursday):
+                        {
+                            delay = 6; break;
+                        }
+                    case (DayOfWeek.Friday):
+                        {
+                            delay = 5; break;
+                        }
+                    case (DayOfWeek.Saturday):
+                        {
+                            delay = 4; break;
+                        }
+                    default:
+                        {
+                            throw new Exception("Sunday is a day off");
+                        }
+                }
+                DateTime processing = date.AddDays(delay);
+                TimeSpan delivering = processing - date;
+
+                Console.WriteLine($"It is {date.DayOfWeek} " +
+                    $"and deliver is {delivering.Days} days " +
+                    $"and it will be {processing.Date}.");
+            }
+        }
+    }
+}
